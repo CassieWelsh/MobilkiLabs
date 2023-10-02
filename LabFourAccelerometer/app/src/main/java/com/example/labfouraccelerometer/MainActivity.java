b8b13c59a9e0e6829f2d2b62bd9fafcb47035614
CassieWelsh <maksim.airapetyan3@yandex.ru>
2023-10-02 20:55:13 +0300

    Accelerator

--- LabFourAccelerometer/app/src/main/java/com/example/labfouraccelerometer/MainActivity.java
+++ LabFourAccelerometer/app/src/main/java/com/example/labfouraccelerometer/MainActivity.java
@@ -1,84 +1,67 @@
 package com.example.labfouraccelerometer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
-    private ImageView movableObject;
-    private SensorManager sensorManager;
-    private Sensor accelerometer;
-    private float lastX, lastY;
-    private boolean firstTime = true;
+
+    private SensorManager _sensorManager;
+    private TextView _square;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        movableObject = findViewById(R.id.movableObject);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        _square = findViewById(R.id.square);
 
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        if (sensorManager != null) {
-            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        setUpSensorStuff();
+    }
+
+    private void setUpSensorStuff() {
+        _sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        _sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        _sensorManager.registerListener(
+                this,
+                _sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_FASTEST,
+                SensorManager.SENSOR_DELAY_FASTEST
+        );
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        // Checks for the sensor we have registered
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float sides = sensorEvent.values[0];
+            float upDown = sensorEvent.values[1];
+
+            _square.setRotationX(upDown * 3f);
+            _square.setRotationY(sides * 3f);
+            _square.setRotation(-sides);
+            _square.setTranslationX(sides * (-10));
+            _square.setTranslationY(upDown * 10);
+
+            // Changes the colour of the square if it's completely flat
+            int color = (Math.round(upDown) == 0 && Math.round(sides) == 0) ? Color.GREEN : Color.RED;
+            _square.setBackgroundColor(color);
+
+            _square.setText("up/down " + Math.round(upDown) + "\n left/right " + Math.round(sides));
         }
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        if (accelerometer != null) {
-            sensorManager.registerListener((SensorEventListener) this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-        }
+    public void onAccuracyChanged(Sensor sensor, int i) {
     }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (sensorManager != null) {
-            sensorManager.unregisterListener(this);
-        }
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            float x = event.values[0];
-            float y = event.values[1];
-
-            if (firstTime) {
-                lastX = x;
-                lastY = y;
-                firstTime = false;
-            }
-
-            float deltaX = x - lastX;
-            float deltaY = y - lastY;
-
-            float newX = movableObject.getX() - deltaX;
-            float newY = movableObject.getY() + deltaY;
-
-            // Ограничьте перемещение объекта в пределах экрана
-            newX = Math.max(0, Math.min(newX, getWindowManager().getDefaultDisplay().getWidth() - movableObject.getWidth()));
-            newY = Math.max(0, Math.min(newY, getWindowManager().getDefaultDisplay().getHeight() - movableObject.getHeight()));
-
-            movableObject.setX(newX);
-            movableObject.setY(newY);
-
-            lastX = x;
-            lastY = y;
-        }
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // Ничего не делаем при изменении точности акселерометра
-    }
-
 }
